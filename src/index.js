a2e89c2b8a1b049efa8933af63f47093748f077b
aziim1284 <aziimbhatti613@gmail.com>
2024-02-01 19:17:27 +0530

    added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter} from 'react-router-dom'
+import ScrollTop from './component/ScrollTop'
+
+import { CartProvider } from "./context/CartCotext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <HashRouter>
+        <ScrollTop />
+        <App />
+      </HashRouter>
+    </CartProvider>
   </React.StrictMode>
 );
 
